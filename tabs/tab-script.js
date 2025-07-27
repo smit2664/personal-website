951e39f3d2f1659f41ea805d8c4bfcfb2a4ea912
smit2664 <smith994c@gmail.com>
2025-07-26 21:06:59 -0400

    Create tab-script.js

---
+++ tabs/tab-script.js
@@ -0,0 +1,4 @@
+function toggleMobileMenu() {
+  const mobileMenu = document.getElementById('mobileMenu');
+  mobileMenu.classList.toggle('show'); // Toggle the class to show/hide the mobile menu
+}
