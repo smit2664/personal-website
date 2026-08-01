df4f1862619924379d2be6409a5be5ddeb40f562
smit2664 <smith994c@gmail.com>
2026-08-01 10:58:04 -0400

    Delete tabs directory

--- tabs/tab-script.js
+++
@@ -1,8 +0,0 @@
-function toggleMobileMenu() {
-  const menu = document.getElementById("mobileMenu");
-  if (menu.style.display === "flex") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "flex";
-  }
-}
